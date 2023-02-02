c54148ff5b8b9a751712e2045e41b1e1a38735de
Sorek11 <92301451+Sorek11@users.noreply.github.com>
2023-02-02 12:05:43 +0100

    Create Colores.java

---
+++ Programacion/Tema 3/Colores.java
@@ -0,0 +1,16 @@
+public class ColoresJava {
+
+    public static void main (String[] args){
+    
+        final String ANSI_NEGRO = "\033[30m";
+        final String ANSI_ROJO = "\033[31m";
+        final String ANSI_AMARILLO = "\033[33m";
+        final String ANSI_AZUL = "\033[34m";
+        final String ANSI_MORADO = "\033[35m";
+        final String ANSI_CYAN = "\033[36m";
+        final String ANSI_BLANCO = "\033[37m";
+        final String ANSI_VERDE = "\033[32m";
+        final String ANSI_RESET = "\033[0m";
+
+    }
+}
