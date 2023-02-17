3d46dd0d76883b322bbd6481bd9fcac88bc11bd6
Víctor García <92301451+vgarciaDev@users.noreply.github.com>
2023-02-17 11:32:57 +0100

    Delete Lenguaje de Marcas directory

--- Lenguaje de Marcas/Conversor € a $/script.js
+++
@@ -1,6 +0,0 @@
-function conversor(){
-  const euro = document.getElementById("euro").value;
-  const dolar = 1.09;
-  var solucion = (euro / dolar);
-  document.getElementById("dolar").innerHTML = Number(solucion.toFixed(2));
-}
